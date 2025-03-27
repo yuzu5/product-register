8a48bcbf127352e35e37c4fc92d438b6c17d1438
yuzu5 <global.aoki08160816@gmail.com>
2025-03-27 22:59:23 +0900

    first commit

---
+++ db/migrate/20250327091633_create_products.rb
@@ -0,0 +1,11 @@
+class CreateProducts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.integer :price
+      t.string :vendor
+
+      t.timestamps
+    end
+  end
+end
